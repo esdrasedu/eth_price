4149f24e04b9f95a4c7aa63319aaca453cead267
Esdras Eduardo <esdrasedu@gmail.com>
2016-03-31 21:04:45 -0300

    fix tests

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -4,25 +4,25 @@ defmodule EthPrice.PageControllerTest do
 
   test "root route", %{conn: conn} do
     conn = get conn, "/"
-    {:safe, input} = text_input(:ethereum, :value, value: 1.0)
+    {:safe, input} = text_input(:ethereum, :value, value: 1.0, class: "price-input")
     assert html_response(conn, 200) =~ input
   end
 
   test "set param value with 1", %{conn: conn}  do
     conn = get conn, "/", %{ethereum: %{value: "1"}}
-    {:safe, input} = text_input(:ethereum, :value, value: 1.0)
+    {:safe, input} = text_input(:ethereum, :value, value: 1.0, class: "price-input")
     assert html_response(conn, 200) =~ input
   end
 
   test "set params value with 1.1", %{conn: conn}  do
     conn = get conn, "/", %{ethereum: %{value: "1.1"}}
-    {:safe, input} = text_input(:ethereum, :value, value: 1.1)
+    {:safe, input} = text_input(:ethereum, :value, value: 1.1, class: "price-input")
     assert html_response(conn, 200) =~ input
   end
 
   test "set params value empty", %{conn: conn}  do
     conn = get conn, "/", %{ethereum: %{value: ""}}
-    {:safe, input} = text_input(:ethereum, :value, value: 1.0)
+    {:safe, input} = text_input(:ethereum, :value, value: 1.0, class: "price-input")
     assert html_response(conn, 200) =~ input
   end
 
