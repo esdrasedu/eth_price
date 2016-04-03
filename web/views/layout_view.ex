3f6599efc06bfc61570d4c348cd4ea848329ea49
Esdras Eduardo <esdrasedu@gmail.com>
2016-04-03 18:29:04 -0300

    add role build and deploy

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -3,7 +3,7 @@ defmodule EthPrice.LayoutView do
 
   def version do
     :application.which_applications
-    |> Enum.find(fn({app, name, version}) -> app == :eth_price end)
+    |> Enum.find(fn({app, _name, _version}) -> app == :eth_price end)
     |> elem(2)
     |> to_string()
   end
