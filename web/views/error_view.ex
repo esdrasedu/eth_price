25a445be15900ba6ab3f13167d7d71045c60cb47
Esdras Eduardo <esdrasedu@gmail.com>
2016-04-04 23:11:24 -0300

    Add page of error

--- web/views/error_view.ex
+++ web/views/error_view.ex
@@ -2,16 +2,16 @@ defmodule EthPrice.ErrorView do
   use EthPrice.Web, :view
 
   def render("404.html", _assigns) do
-    "Page not found"
+    render("default.html", %{error: "Where's my page?"})
   end
 
   def render("500.html", _assigns) do
-    "Server internal error"
+    render("default.html", %{error: "What's happening?"})
   end
 
   # In case no render clause matches or no
   # template is found, let's render it as 500
   def template_not_found(_template, assigns) do
-    render "500.html", assigns
+    render "default.html", %{error: "Where's my template?"}
   end
 end
